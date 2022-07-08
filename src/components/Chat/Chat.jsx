b7676e053daa96ad5536bb2f39b9acbc7b5a7139
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-08 15:33:05 -0400

    Create Chat.jsx

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,10 @@
+<!-- Chat Window -->
+<div class="chat-notification">
+  <div class="chat-notification-logo-wrapper">
+    <img class="chat-notification-logo" src="/img/logo.svg" alt="Chat Logo">
+  </div>
+  <div class="chat-notification-content">
+    <h4 class="chat-notification-title">Chat</h4>
+    <p class="chat-notification-message">You have a new message!</p>
+  </div>
+</div>
