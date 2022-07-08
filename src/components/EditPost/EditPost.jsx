2685c5ca4e734cb3925e1481ec454905599ebd47
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-08 12:13:39 -0400

    Update EditPost.jsx

--- src/components/EditPost/EditPost.jsx
+++ src/components/EditPost/EditPost.jsx
@@ -1,1 +1,67 @@
+//edit posts function
 
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const postId = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+}
+//Post Title & Post Text
+
+const title = document.querySelector('input[name="post-title"]').value;
+const post_container = document.querySelector(
+  'textarea[name="post-container"]'
+).value;
+
+// use the route to update the post
+
+const response = await fetch(`/api/posts/${postId}`, {
+  method: "PUT",
+  body: JSON.stringify({
+    title,
+    post_container,
+  }),
+  headers: { "Content-Type": "application/json" },
+});
+// If the post edited successfully display in dashboard
+// If not display error message
+
+if (response.ok) {
+  document.location.replace("/dashboard");
+
+  //display an error
+} else {
+  alert(response.statusText);
+}
+
+//function to delete post
+
+async function deleteClickHandler(event) {
+  event.preventDefault();
+
+  //get post id from url
+  const postId = window.location.toString().split("/")[
+    window.location.toString().split("/").lenght - 1
+  ];
+  //using async function to delete the post
+
+  const response = await fetch(`/api/post/${postId}`, {
+    method: "DELETE",
+  });
+
+  //If post deleted successfully display dashboard
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
+
+document
+  .querySelector(".delete-post-button")
+  .addEventListener("click", deleteClickHandler);
