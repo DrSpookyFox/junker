2b95458da4f8e5d649df75d32e6af79dbd95d2d4
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-08 12:19:26 -0400

    Update EditPost.jsx

--- src/components/EditPost/EditPost.jsx
+++ src/components/EditPost/EditPost.jsx
@@ -44,24 +44,7 @@ async function deleteClickHandler(event) {
   const postId = window.location.toString().split("/")[
     window.location.toString().split("/").lenght - 1
   ];
-  //using async function to delete the post
-
-  const response = await fetch(`/api/post/${postId}`, {
-    method: "DELETE",
-  });
-
-  //If post deleted successfully display dashboard
-  if (response.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    alert(response.statusText);
-  }
-}
-
+  
 document
   .querySelector(".edit-post-form")
   .addEventListener("submit", editFormHandler);
-
-document
-  .querySelector(".delete-post-button")
-  .addEventListener("click", deleteClickHandler);
