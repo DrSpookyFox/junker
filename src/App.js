dc5d6bdd6527fc4ea441613187ee9d80a5074a88
Jmorg21 <Jmorg21@gmail.com>
2022-07-07 20:49:21 -0400

    Added TailwindCSS

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{css,js,svg}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
