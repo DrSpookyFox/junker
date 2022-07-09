3f90a2acda82264b033a3dd8cb7807320c975f13
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-09 11:37:56 -0400

    Create tailwind.config.js

---
+++ config/tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    extend: {},
+  },
+  plugins: [],
+},
+};
